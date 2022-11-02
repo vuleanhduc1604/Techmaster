da70c7d972fb32f2c241ca89c83dca98e3018ed1
justnothin <vuleanhduc1604@gmail.com>
2022-11-02 21:25:35 +0700

    update project

--- B13-reactjs/note.js
+++ B13-reactjs/note.js
@@ -1,3 +1,10 @@
-// Props là read onlu (không thể thay đổi)
-// Có mố số props đặc biệt
+// Props là read only (không thể thay đổi)
+// Có một số props đặc biệt
 // defaultProps => Cung cấp các giá trị mặc định cho props trong trường học ko load đc
+
+
+
+// BTVN:
+// 1. Tìm hiểu về xử lý sự kiện trong React
+// 2. Tìm hiểu về `key` khi sử dụng `map`
+// 3. Tìm hiểu về hook và `useState`
